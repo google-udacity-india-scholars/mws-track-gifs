f95580fec42b575c1155a7ed9beeda5ec2123dba
unknown <istiaquehaque@gmail.com>
2018-10-27 00:20:40 +0530

    feat: Added missing updated index.html

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
      */
     static get API_KEY() {
         // add api key
-        const apiKey = `Mv8cx0jg46ti65rm8sbK0BECcCY5f2ve&q`;
+        const apiKey = `YOUR_API_KEY`;
         return apiKey;
     }
 
